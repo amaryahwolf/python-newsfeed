d1ca359a16526bcc51537fb18f2bb6bcb36acd17
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-03-20 15:42:04 -0700

    set up database queries, manage db connections using Flask app context, import custom filters into a Jinja template

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,25 @@
 from app.routes import home, dashboard
 # Use from ... import statement to import the Flask() function
 from flask import Flask
+from app.db import init_db
+from app.utils import filters
 
 # Use def keyword to define a create_app() function
 # Indent code blocks by two spaces to contain within function
 def create_app(test_config=None):
   # set up app config
-#   App should serve any static resources from the root directory and not from the default /static directory
+  # App should serve any static resources from the root directory and not from the default /static directory
   app = Flask(__name__, static_url_path='/')
-#   Trailing slashes are optional
+  # Trailing slashes are optional
   app.url_map.strict_slashes = False
-#   App should use the key when creating server-side sessions
+  # App should use the key when creating server-side sessions
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+  # Register filter functions
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 #   Decorator turns the function into a route
   @app.route('/hello')
     # Inner function that returns a string
@@ -24,5 +30,7 @@ def create_app(test_config=None):
   # Register home blueprint
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  init_db(app)
 
   return app
